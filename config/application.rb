a674e68fe0e0ebd1a02e6431e4f9539fc1158a0b
kwonkers <kwonkers@gmail.com>
2019-02-21 20:54:38 +0000

    Finish static pages

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,12 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
